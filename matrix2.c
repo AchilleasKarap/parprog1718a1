1f1d24cf7432967138c07269f54daf3e69efd848
Karapiperis Achilleas <achilleask10@gmail.com>
2018-03-13 11:31:41 +0200

    Update matrix2.c

--- matrix2.c
+++ matrix2.c
@@ -1,54 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-
-// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix2.c -o matrix2
+#include <time.h>
+// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix1.c -o matrix1
 
 
 #define NCOLS 100
+#define NROWS 100
+
 
 void get_walltime(double *wct) {
-  struct timeval tp;
-  gettimeofday(&tp,NULL);
-  *wct = (double)(tp.tv_sec+tp.tv_usec/1000000.0);
+    struct timeval tp; //domh tou system
+    gettimeofday(&tp, NULL);
+    *wct = (double) (tp.tv_sec + (tp.tv_usec / 1000000.0));
 }
-
 
 int main() {
-double *table;
-double ts,te;
+    double i, j, M, N;
+    double ts, te, *table;
 
+	
+	 table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
+	if (table==NULL) {
+		printf("alloc error!\n");
+		exit(1);
+	}
+	
+	M=100;
+	N=100;
+    get_walltime(&ts);
+	for (j = 1; j < N - 1; j++) {//einai oi sthles ?
+		for (i = 1; i < M - 1; i++) {//einai oi grammes ?
+			*table=5;
+        }
+    }
+    get_walltime(&te);
 
-  table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
-  if (table==NULL) {
-    printf("alloc error!\n");
-    exit(1);
-  }
-
-  // warmup
-
-  // ...your code here...
-
-  // get starting time (double, seconds) 
-  get_walltime(&ts);
-  
-  // workload
-
-  // ...your code here...
-
-  // get ending time
-  get_walltime(&te);
-
-  // check results
-  
-  // ...your code here...
-
-  // print time elapsed and/or Maccesses/sec
-  
-  // ...your code here...  
-  
-  free(table);
-
-  return 0;
+    printf("Done on %lf seconds\n", (te - ts)); //emfanish tou xronou pou xriasthke gia na treksei to programma
+	free(table);   
+	return 0;
 }
-
